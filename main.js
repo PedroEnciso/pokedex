5be0aa42f06a1595f84fd5cf78b4da074d743d9a
penciso1 <63486016+penciso1@users.noreply.github.com>
2021-02-13 01:01:13 -0800

    started styling and adding html

--- main.js
+++ main.js
@@ -1,14 +1,21 @@
 
-let front = document.getElementById('pokeFront');
+let front = document.getElementById('sprite1');
 let url = "https://pokeapi.co/api/v2/pokemon/";
+let desc = document.getElementById('description');
 
-let pokemonNumber = 300;
+let pokemonNumber = 1;
 
 fetch(url + pokemonNumber)
     .then(function(resp) {
         return resp.json();
     })
     .then(function(data) {
-        console.log(data.types);
         front.src = data.sprites.front_default;
+        fetch("https://pokeapi.co/api/v2/pokemon-species/" + pokemonNumber)
+            .then(function(resp) {
+                return resp.json();
+            })
+            .then(function(data2) {
+                description.innerHTML = data2.flavor_text_entries[0].flavor_text
+            })
     })
