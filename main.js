b95ce541ad227ff1f5ce96e3bfa66935a1f795ac
penciso1 <63486016+penciso1@users.noreply.github.com>
2021-02-15 09:41:19 -0800

    created styling for top screen

--- main.js
+++ main.js
@@ -1,21 +1,33 @@
 
-let front = document.getElementById('sprite1');
 let url = "https://pokeapi.co/api/v2/pokemon/";
 let desc = document.getElementById('description');
 
-let pokemonNumber = 1;
+let pokemonNumber = 10;
 
-fetch(url + pokemonNumber)
+let sprite;
+let pokeNumber;
+let pokeName;
+
+for (let i = 1; i <= pokemonNumber; i++)
+fetch(url + i)
     .then(function(resp) {
         return resp.json();
     })
     .then(function(data) {
-        front.src = data.sprites.front_default;
-        fetch("https://pokeapi.co/api/v2/pokemon-species/" + pokemonNumber)
+        sprite = document.getElementById('sprite' + i)
+        pokeNumber = document.getElementById('pokeNumber' + i)
+        pokeName = document.getElementById('pokeName' + i)
+
+
+        pokeNumber.innerHTML = '# ' + i;
+        sprite.src = data.sprites.front_default;
+        pokeName.innerHTML = data.name;
+        /*
+        fetch("https://pokeapi.co/api/v2/pokemon-species/" + i)
             .then(function(resp) {
                 return resp.json();
             })
             .then(function(data2) {
                 description.innerHTML = data2.flavor_text_entries[0].flavor_text
-            })
+            })*/
     })
