83ce2c3d358f391e6b2a8adcbb5b3f272dacb3bc
penciso1 <63486016+penciso1@users.noreply.github.com>
2021-02-15 23:28:19 -0800

    tried to fix functionality, didnt work

--- main.js
+++ main.js
@@ -7,29 +7,31 @@ let listNumber = 0;
 let sprite;
 let pokeNumber;
 let pokeName;
+let pokemonNumber;
 
-for (let i = 1; i <= 10; i++)
+for (let i = 1; i <= 10; i++){
 
-pokemonNumber = i + listNumber;
-fetch(url + pokemonNumber)
-    .then(function(resp) {
-        return resp.json();
-    })
-    .then(function(data) {
-        sprite = document.getElementById('sprite' + pokemonNumber)
-        pokeNumber = document.getElementById('pokeNumber' + pokemonNumber)
-        pokeName = document.getElementById('pokeName' + pokemonNumber)
+    pokemonNumber = i + listNumber;
+    fetch(url + pokemonNumber)
+        .then(function(resp) {
+            return resp.json();
+        })
+        .then(function(data) {
+            sprite = document.getElementById('sprite' + pokemonNumber)
+            pokeNumber = document.getElementById('pokeNumber' + pokemonNumber)
+            pokeName = document.getElementById('pokeName' + pokemonNumber)
 
 
-        pokeNumber.innerHTML = '# ' + pokemonNumber;
-        sprite.src = data.sprites.front_default;
-        pokeName.innerHTML = data.name;
-        /*
-        fetch("https://pokeapi.co/api/v2/pokemon-species/" + i)
-            .then(function(resp) {
-                return resp.json();
-            })
-            .then(function(data2) {
-                description.innerHTML = data2.flavor_text_entries[0].flavor_text
-            })*/
-    })
+            pokeNumber.innerHTML = '# ' + pokemonNumber;
+            sprite.src = data.sprites.front_default;
+            pokeName.innerHTML = data.name;
+            /*
+            fetch("https://pokeapi.co/api/v2/pokemon-species/" + i)
+                .then(function(resp) {
+                    return resp.json();
+                })
+                .then(function(data2) {
+                    description.innerHTML = data2.flavor_text_entries[0].flavor_text
+                })*/
+        })
+}
