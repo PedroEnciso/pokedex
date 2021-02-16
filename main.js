3d179ac26b35f9a8b31488bcf23585431671d5bc
penciso1 <63486016+penciso1@users.noreply.github.com>
2021-02-16 09:42:30 -0800

    added button finctionality and edge cases

--- main.js
+++ main.js
@@ -2,36 +2,65 @@
 let url = "https://pokeapi.co/api/v2/pokemon/";
 let desc = document.getElementById('description');
 
-let listNumber = 0;
+let listNumber = 860;
 
 let sprite;
 let pokeNumber;
 let pokeName;
 let pokemonNumber;
 
-for (let i = 1; i <= 10; i++){
-
+for (let i = 1; i <= 10; i++) {
     pokemonNumber = i + listNumber;
+    console.log(pokemonNumber + " and i = " + i);
+    getPokemon(pokemonNumber, i);
+}
+
+
+function getPokemon(pokemonNumber, i) {
     fetch(url + pokemonNumber)
-        .then(function(resp) {
-            return resp.json();
-        })
-        .then(function(data) {
-            sprite = document.getElementById('sprite' + pokemonNumber)
-            pokeNumber = document.getElementById('pokeNumber' + pokemonNumber)
-            pokeName = document.getElementById('pokeName' + pokemonNumber)
+    .then(function(resp) {
+        return resp.json();
+    })
+    .then(function(data) {
+        sprite = document.getElementById('sprite' + i)
+        pokeNumber = document.getElementById('pokeNumber' + i)
+        pokeName = document.getElementById('pokeName' + i)
 
 
-            pokeNumber.innerHTML = '# ' + pokemonNumber;
-            sprite.src = data.sprites.front_default;
-            pokeName.innerHTML = data.name;
-            /*
-            fetch("https://pokeapi.co/api/v2/pokemon-species/" + i)
-                .then(function(resp) {
-                    return resp.json();
-                })
-                .then(function(data2) {
-                    description.innerHTML = data2.flavor_text_entries[0].flavor_text
-                })*/
-        })
+        pokeNumber.innerHTML = '# ' + pokemonNumber;
+        sprite.src = data.sprites.front_default;
+        pokeName.innerHTML = data.name;
+        /*
+        fetch("https://pokeapi.co/api/v2/pokemon-species/" + i)
+            .then(function(resp) {
+                return resp.json();
+            })
+            .then(function(data2) {
+                description.innerHTML = data2.flavor_text_entries[0].flavor_text
+            })*/
+    })
+}
+
+function getNextList() {
+    if (listNumber == 890) {return}
+    listNumber += 10;
+    for (let i = 1; i <= 10; i++) {
+        pokemonNumber = i + listNumber;
+        if (pokemonNumber > 898) {
+            getPokemon(1, i)
+        }
+        console.log(pokemonNumber + " and i = " + i);
+        getPokemon(pokemonNumber, i);
+    }
+}
+
+
+function getPrevList() {
+    if (listNumber == 0) { return }
+    listNumber -= 10;
+    for (let i = 1; i <= 10; i++) {
+        pokemonNumber = i + listNumber;
+        console.log(pokemonNumber + " and i = " + i);
+        getPokemon(pokemonNumber, i);
+    }
 }
