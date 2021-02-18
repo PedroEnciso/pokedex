2f48de33ee40c2747ab74ca50ce6fb021c450ee1
penciso1 <63486016+penciso1@users.noreply.github.com>
2021-02-18 12:12:07 -0800

    get styling from api

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 //get array of every pokemon name
+/*
 fetch("https://pokeapi.co/api/v2/pokemon/?limit=898")
     .then( resp => resp.json())
     .then( data => {
@@ -8,23 +9,31 @@ fetch("https://pokeapi.co/api/v2/pokemon/?limit=898")
             pokemonNameArray[i] = pokemonNames[i].name
         }
     })
-
+    */
 let pokeImage = document.getElementById('pokeImage')
 let pokeType1 = document.getElementById('pokeType1')
 let pokeType2 = document.getElementById('pokeType2')
+let type1;
 
 
-fetch("https://pokeapi.co/api/v2/pokemon/steelix/")
+fetch("https://pokeapi.co/api/v2/pokemon/87/")
     .then( resp => resp.json())
     .then( data => {
+        document.getElementById('pokeName').innerHTML = data.name
         pokeImage.src = data.sprites.other.dream_world.front_default
-        console.log(data.types[0])
-        pokeType1.innerHTML = data.types[0].type.name
+        type1 = data.types[0].type.name
+        pokeType1.innerHTML = type1
+        document.getElementById('type1-container').classList.add(type1)
+        document.getElementById('background').classList.add(`bg-${type1}`)
+        if (data.types.length > 1) {
+            pokeType2.innerHTML = data.types[1].type.name
+            document.getElementById('type2-container').classList.add(data.types[1].type.name)
+        }
     })
 
 let pokeDescription = document.getElementById('pokeDescription')
  
-fetch("https://pokeapi.co/api/v2/pokemon-species/blaziken/")
+fetch("https://pokeapi.co/api/v2/pokemon-species/87/")
     .then( resp => resp.json())
     .then( data => {
         pokeDescription.innerHTML = data.flavor_text_entries[0].flavor_text
